2e8110d2031eebfd4743eeaf2249997b78215be1
sinaKafi <sina.sk.kafi@gmail.com>
2024-11-29 01:17:19 +0330

    images fiixed

--- src/components/carosel/Slider.tsx
+++ src/components/carosel/Slider.tsx
@@ -1,39 +1,3 @@
-// "use client";
-// import { sliderSettings } from "@/utils/slider";
-// import Image from "next/image";
-// import Slider from "react-slick";
-
-// const ImageSlider = ({ array = [] }: { array?: string[] }) => {
-//   const totalImages = array.length;
-
-//   const dynamicSliderSettings = {
-//     ...sliderSettings,
-//     infinite: totalImages > 1, // Disable infinite loop when there's only one image
-//     swip: totalImages > 1,
-//     slidesToShow: totalImages == 3 ? 2.5 : totalImages, // Adjust dynamically based on the number of images
-//     centerMode: totalImages == 1,
-//   };
-
-//   return (
-//     <div>
-//       <Slider {...dynamicSliderSettings}>
-//         {array?.map((image) => (
-//           <div className="!appearance-none p-2 outline-none   " key={image}>
-//             <Image
-//               src={image}
-//               alt="Image Description"
-//               className="mx-auto aspect-square object-fill"
-//               width={550}
-//               height={550}
-//             />
-//           </div>
-//         ))}
-//       </Slider>
-//     </div>
-//   );
-// };
-
-// export default ImageSlider;
 "use client";
 import { sliderSettings } from "@/utils/slider";
 import Image from "next/image";
@@ -58,7 +22,7 @@ const ImageSlider = ({ array = [] }: { array?: string[] }) => {
             className="w-full !appearance-none p-2 outline-none "
             key={image}
           >
-            <div className="max-w-[25rem] max-h-[25rem] relative  aspect-square w-full mx-auto bg-gray-200 overflow-hidden rounded-lg">
+            <div className="max-w-[20rem] max-h-[20rem] relative  aspect-square w-full mx-auto bg-gray-200 overflow-hidden rounded-lg">
               <Image
                 src={image}
                 alt="Image Description"
