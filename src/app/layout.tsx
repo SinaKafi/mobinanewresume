a279d26c58d6158c34fddab51fb1ffe676ae996d
sina <sk@sinas-Laptop.local>
2025-01-02 17:08:33 +0330

    add meta open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "/mobi.png",
+        url: "https://www.mobinamoghadam.art/mobi.png",
         width: 1200,
         height: 630,
         alt: "Mobina Moghadam Portfolio",
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     ],
   },
 
-  themeColor: "#7b7b7b",
+  themeColor: "#dddddd",
 };
 
 export default function RootLayout({
@@ -38,32 +38,41 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Favicon */}
         <link rel="icon" type="image/x-icon" href="/vector.svg" />
+
+        {/* Meta Tags for Basic Setup */}
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
         />
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="theme-color" content="#7b7b7b" />
+        <meta name="theme-color" content="#dddddd" />
 
+        {/* Cache Control (For better performance) */}
         <meta
           httpEquiv="Cache-Control"
           content="no-cache, no-store, must-revalidate"
         />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
-      </head>
 
-      <body className="antialiased  flex flex-col w-full  min-h-screen  bg-background  ">
-        <link rel="icon" type="image/x-icon" href="/vector.svg" />
+        {/* SEO Meta Tags */}
         <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          name="description"
+          content="UI/UX Designer Portfolio - Mobina Moghadam"
         />
-        {/*  */}
+        <meta
+          name="keywords"
+          content="Mobina Moghadam, UI/UX Designer, Portfolio, Creative Design, Web Design, User Experience"
+        />
+        <meta name="author" content="Mobina Moghadam" />
+        <meta name="robots" content="index, follow" />
+
+        {/* Open Graph Meta Tags for Social Sharing */}
         <meta property="og:title" content="Mobina Moghadam | Portfolio" />
         <meta
           property="og:description"
@@ -71,26 +80,22 @@ export default function RootLayout({
         />
         <meta property="og:url" content="https://www.mobinamoghadam.art" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://www.mobinamoghadam.art" />
+        <meta
+          property="og:image"
+          content="https://www.mobinamoghadam.art/mobi.png"
+        />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Mobina Moghadam Portfolio" />
-        {/*  */}
-        <meta
-          name="keywords"
-          content="Mobina Moghadam, UI/UX Designer, Portfolio, Creative Design, Web Design, User Experience"
-        />
-        <meta name="author" content="Mobina Moghadam" />
-        <meta
-          name="description"
-          content="UI/UX Designer Portfolio - Mobina Moghadam"
-        />
-        <meta
-          httpEquiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta name="robots" content="index, follow" />
 
+        {/* Canonical Link for SEO */}
+        <link rel="canonical" href="https://www.mobinamoghadam.art" />
+
+        {/* Set the Theme Color  */}
+        <meta name="theme-color" content="#dddddd" />
+      </head>
+
+      <body className="antialiased  flex flex-col w-full  min-h-screen  bg-background  ">
         <div className="hidden md:block md:w-full md:h-14 xl:h-20"></div>
         <Header />
 
